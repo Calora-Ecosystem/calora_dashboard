3b3fa062b11f02d7dcd1128fec3113141ebdb862
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2026-05-20 12:48:17 +0500

    feat: add kcal field to ExerciseEdit and WorkoutEdit components with validation

--- src/views/course/WorkoutEdit.vue
+++ src/views/course/WorkoutEdit.vue
@@ -47,6 +47,7 @@ const data = reactive<{
     activity: ActivityType;
     computationType: ComputationType;
     value: number;
+    kcal: number;
   }[];
 }>({
   title: { uz: "", ru: "", eng: "" },
@@ -60,6 +61,7 @@ const data = reactive<{
     activity: x,
     computationType: COMPUTATION_TYPE[0],
     value: 0,
+    kcal: 0,
   })) as [],
 });
 
@@ -101,6 +103,7 @@ const rules = reactive<FormRules<typeof data>>({
           enum: COMPUTATION_TYPE as any,
         },
         value: { required: true, type: "number" },
+        kcal: { required: true, type: "number" },
       },
     },
   },
@@ -225,7 +228,7 @@ onMounted(async () => {
         </div>
       </ElFormItem>
 
-<!--      <div class="flex flex-row justify-start gap-x-3 min-w-0 overflow-x-auto">
+      <!--      <div class="flex flex-row justify-start gap-x-3 min-w-0 overflow-x-auto">
         <ElFormItem label="Main Image" required prop="assets.0.url">
           <FileUpload v-model="data.assets[0].url" />
         </ElFormItem>
